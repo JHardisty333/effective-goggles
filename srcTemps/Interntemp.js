bd755653c84b6054a75a565fc12af2606722847f
JHardisty333 <jessica1990_37@hotmail.com>
2021-10-09 16:23:37 -0600

    role cards

---
+++ srcTemps/Interntemp.js
@@ -0,0 +1,17 @@
+function addIntern(Intern){
+    return `
+    <div class="col">
+    <div class="card" style="width: 18rem;">
+        <div class="card-header bg-success">
+            <h4 class="text-light">${Intern.name}</h4>
+            <h5><i class="bi bi-eyeglasses"></i>Intern</h5>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">${Intern.id}</li>
+            <li class="list-group-item">${Intern.roleDetails} </li>
+            <a href="mailto:${Intern.email}" class="btn btn-secondary">${Intern.email}</a>
+        </ul>
+    </div>
+    </div>
+    `
+}
